79603aa1b6086004edff530e17cc9161ef3aa9d1
Geronimo Perez <geronimo.perez4@gmail.com>
2021-03-14 21:18:54 -0400

    added matplotlib to repos

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+// var width = 400
+// var height = .66*width
+// var svg = d3.select('#svg').append('svg').style('height',height).style('width',width);
+
+// var cal = svg
+//     .append('rect')
+//     .style('width',width)
+//     .style('height',height)
+//     .attr('rx','20')
+//     .attr('ry','20')
+//     // .attr('y',50)
+//     // .attr('x',50)
+//     .attr('fill','lightblue');
+
+// // svg.append('rect').attr('fill','white').style('height',.9*height).style('width',.10*width)
+
+// var text = svg.append('g');
+
