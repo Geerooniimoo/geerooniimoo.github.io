2770cd0b7d66fd760818b301067c9f18ac46a5b2
Geronimo Perez <geronimo.perez4@gmail.com>
2023-01-28 09:01:05 -0500

    completed large desktop view of the app

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,35 +85,51 @@ function handleForward() {
 // span.onclick = () => modal.style.display = "none";
 // window.onclick = event => event.target == modal ? modal.style.display = "none":"";
 
-var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
 
 // Get the button that opens the modal
-var btn = document.getElementById("sale");
+var btn1 = document.getElementById("sale");
+var btn2 = document.getElementById("analyst");
+var btn3 = document.getElementById("developer");
 
 // Get the <span> element that closes the modal
 var span = document.querySelector(".close")
 
 // When the user clicks on the button, open the modal
-btn.onclick = () => {
-    let y = btn.getBoundingClientRect().y, x = btn.getBoundingClientRect().x;
-    btn.style = `top: ${y}px;left: ${x}px;animation: sale2 .5s`;
+btn1.onclick = () => {
+    let y = btn1.getBoundingClientRect().y, x = btn1.getBoundingClientRect().x;
+    btn1.style = `top: ${y}px;left: ${x}px;animation: sale2 .5s`;
 
     setTimeout(()=>{
-        btn.style = 'display:none';
-        modal.style.display = "block";
+        btn1.style = 'display:none';
+        modal1.style.display = "block";
     },500)
-}
+};
+
+btn2.onclick = () => {
+        modal2.style.display = "block";
+};
+
+btn3.onclick = () => {
+        modal3.style.display = "block";
+};
 
 span.onclick = function() {
-    btn.style = 'display:block;animate: sale 50s infinite';
-    modal.style.display = "none";
+    btn1.style = 'display:block;animate: sale 50s infinite';
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     console.log(event.target);
-    if (event.target == modal) {
-    btn.style = 'display:block;animate: sale 50s infinite';
-    modal.style.display = "none";
+    if (event.target == modal1 || event.target == modal2 || event.target == modal3 ) {
+    btn1.style = 'display:block;animate: sale 50s infinite';
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
   }
 }
