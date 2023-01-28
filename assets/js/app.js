dd3345dd64b1bb609e27886f4ba2b1dd0b21fc9b
Geronimo Perez <geronimo.perez4@gmail.com>
2023-01-28 00:07:24 -0500

    completed sale button motion

--- assets/js/app.js
+++ assets/js/app.js
@@ -77,3 +77,43 @@ function handleForward() {
         document.querySelector('.type').innerHTML = '',
         handleSpeech()) : '';
 };
+
+// var span = document.querySelector(".close");
+// var modal = document.getElementById("myModal");
+
+// sale.onclick = () => modal.style.display = "block";
+// span.onclick = () => modal.style.display = "none";
+// window.onclick = event => event.target == modal ? modal.style.display = "none":"";
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("sale");
+
+// Get the <span> element that closes the modal
+var span = document.querySelector(".close")
+
+// When the user clicks on the button, open the modal
+btn.onclick = () => {
+    let y = btn.getBoundingClientRect().y, x = btn.getBoundingClientRect().x;
+    btn.style = `top: ${y}px;left: ${x}px;animation: sale2 .5s`;
+
+    setTimeout(()=>{
+        btn.style = 'display:none';
+        modal.style.display = "block";
+    },500)
+}
+
+span.onclick = function() {
+    btn.style = 'display:block;animate: sale 50s infinite';
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    console.log(event.target);
+    if (event.target == modal) {
+    btn.style = 'display:block;animate: sale 50s infinite';
+    modal.style.display = "none";
+  }
+}
