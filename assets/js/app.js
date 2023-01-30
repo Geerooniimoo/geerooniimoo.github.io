a35d14857342e4c752c3753a42a47aaf9c5a7ae0
Geronimo Perez <geronimo.perez4@gmail.com>
2023-01-30 00:23:23 -0500

    completed the first phase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,8 @@
-// document.body.scrollIntoView();
-
-// let type = () => {
-//     let i = 0;
-//     type.innerText = speech;
-// };
-
-// setTimeout(() => {
-//     if (window.screen.width > 576) {
-//         document.querySelector('.accordion').scrollIntoView();
-//     }
-// }, 9800);
 
 let imgId;
 let speechId;
 let speechIndex = 0;
-// let speechId = setTimeout(handleSpeech, 11500);
+
 handleSpeech();
 
 function handleSpeech() {
@@ -26,7 +14,6 @@ function handleSpeech() {
     images.length
         ? (
             !carousel.classList.contains('carousel') ? carousel.classList.toggle('carousel') : '',
-            // document.querySelector('.caroImg').style['-webkit-animation-duration'] = images.length*3+5+'s',
             carousel.innerHTML = `<img class="caroImg">`,
             website ? carousel.innerHTML += `<a target="blank_" href="${website}">Live Website</a>` : '',
             changeImg()
@@ -78,26 +65,14 @@ function handleForward() {
         handleSpeech()) : '';
 };
 
-// var span = document.querySelector(".close");
-// var modal = document.getElementById("myModal");
+let btn1 = document.getElementById("sale");
+let btn2 = document.getElementById("analyst");
+let btn3 = document.getElementById("developer");
+let modal1 = document.getElementById("myModal1");
+let modal2 = document.getElementById("myModal2");
+let modal3 = document.getElementById("myModal3");
 
-// sale.onclick = () => modal.style.display = "block";
-// span.onclick = () => modal.style.display = "none";
-// window.onclick = event => event.target == modal ? modal.style.display = "none":"";
 
-var modal1 = document.getElementById("myModal1");
-var modal2 = document.getElementById("myModal2");
-var modal3 = document.getElementById("myModal3");
-
-// Get the button that opens the modal
-var btn1 = document.getElementById("sale");
-var btn2 = document.getElementById("analyst");
-var btn3 = document.getElementById("developer");
-
-// Get the <span> element that closes the modal
-var span = document.querySelector(".close")
-
-// When the user clicks on the button, open the modal
 btn1.onclick = () => {
     let y = btn1.getBoundingClientRect().y, x = btn1.getBoundingClientRect().x;
     btn1.style = `top: ${y}px;left: ${x}px;animation: sale2 .5s`;
@@ -108,25 +83,16 @@ btn1.onclick = () => {
     },500)
 };
 
-btn2.onclick = () => {
-        modal2.style.display = "block";
-};
+btn2.onclick = () => modal2.style.display = "block";
+btn3.onclick = () => modal3.style.display = "block";
 
-btn3.onclick = () => {
-        modal3.style.display = "block";
-};
-
-span.onclick = function() {
-    btn1.style = 'display:block;animate: sale 50s infinite';
-    modal1.style.display = "none";
-    modal2.style.display = "none";
-    modal3.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    console.log(event.target);
-    if (event.target == modal1 || event.target == modal2 || event.target == modal3 ) {
+window.onclick = e => {
+    console.log(e.target);
+    if (e.target == modal1 || 
+        e.target == modal2 || 
+        e.target == modal3 || 
+        e.target.classList.value == "close"
+    ) {
     btn1.style = 'display:block;animate: sale 50s infinite';
     modal1.style.display = "none";
     modal2.style.display = "none";
