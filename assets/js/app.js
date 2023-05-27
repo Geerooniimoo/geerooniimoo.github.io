ea20145f7656a47b09e564e381eb8cebf442d8db
Geronimo Perez <geronimo.perez4@gmail.com>
2023-05-27 16:42:26 -0400

    added sillabus

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,7 +49,6 @@ function handleRewind() {
         speechIndex -= 2,
         clearTimeout(imgId),
         clearInterval(speechId),
-        console.log(speechIndex),
         speechTitle.innerText = '',
         document.querySelector('.type').innerHTML = '',
         handleSpeech()) : speechIndex = 0;
@@ -57,7 +56,6 @@ function handleRewind() {
 
 function handleForward() {
     speechIndex < speech.length ? (
-        console.log(speechIndex),
         clearTimeout(speechId),
         clearInterval(imgId),
         speechTitle.innerText = '',
@@ -65,96 +63,206 @@ function handleForward() {
         handleSpeech()) : '';
 };
 
-let modal1 = document.getElementById("myModal1");
-let modal2 = document.getElementById("myModal2");
-let modal3 = document.getElementById("myModal3");
-
-
-
-// btn2.onclick = () => modal2.style.display = "block";
-// btn3.onclick = () => modal3.style.display = "block";
-
-window.onclick = e => {
-    console.log(e.target);
-    if (e.target == modal1 || 
-        e.target == modal2 || 
-        e.target == modal3 || 
-        e.target.classList.value == "close"
+window.onclick = ({target}) => {
+    if (target == myModal1 ||
+        target == myModal2 ||
+        target == myModal3 ||
+        target.classList.value == "close"
     ) {
-    // btn1.style = 'display:block;animate: sale 50s infinite';
-    modal1.style.display = "none";
-    modal2.style.display = "none";
-    modal3.style.display = "none";
-  }
-}
+        // btn1.style = 'display:block;animate: sale 50s infinite';
+        myModal1.style.display = "none";
+        myModal2.style.display = "none";
+        myModal3.style.display = "none";
+    };
+
+    if(target== syllabusBtn) {
+        showSyllabus();
+    } else {
+        document.querySelector('#syllabus').style.transform = 'scaleY(0%)'
+        setTimeout(() => {
+            document.getElementById('syllabus').innerHTML = '';
+        }, 1000);
+    };
+};
 
 // const handleModal = modal => console.log(modal);
 const handleModal = modal => {
     scheduleBtn.style.transform = 'scale(.9)';
-    setTimeout(()=>scheduleBtn.style.transform = 'scale(1)', 250);
+    setTimeout(() => scheduleBtn.style.transform = 'scale(1)', 250);
     document.getElementById(modal).style.display = 'block';
 };
 
 const handleOffer = e => {
-    
+
     ['#class35', '#class65', '#class250']
         .forEach(e => {
             document.querySelector(e).style.filter = 'contrast(0)';
             document.querySelector(e).style = 'text-shadow:none;background:none';
-    });
+        });
 
     e.style.transform = 'scale(.9)';
     e.style.textShadow = '0px 0px 5px black, -2px 2px 1px silver';
     e.style.background = e.id == 'class35' ? '#7e2832' : e.id == 'class65' ? '#007d7d' : '#0277bd';
-    setTimeout(()=> {e.style.transform = 'scale(1)'}, 250)
+    setTimeout(() => { e.style.transform = 'scale(1)' }, 250)
 
     e.style.filter = 'contrast(1)'
     document.querySelectorAll('input[type="radio"]').forEach(el => {
         el.disabled = true;
         el.style = 'transform:scale(1)'
     });
-    
+
     let radioList = document.querySelectorAll(`.${e.id}`);
 
-    radioList.forEach((el,i) => {
-        setTimeout(()=> {
-            el.disabled=false;
-            el.checked=true;
+    radioList.forEach((el, i) => {
+        setTimeout(() => {
+            el.disabled = false;
+            el.checked = true;
             el.style = 'transform:scale(1.6)'
-        }, i*200);
+        }, i * 200);
         // setTimeout(()=> {el.style = 'transform:scale(.4)'}, i*200+500);
     });
 
-    setTimeout(()=>{
-        let offer = e.id=='class35' ? '1' :  e.id=='class65' ? '2' : 3;
+    setTimeout(() => {
+        let offer = e.id == 'class35' ? '1' : e.id == 'class65' ? '2' : 3;
         scheduleBtnDiv.innerHTML = `<button id="scheduleBtn" onclick="handleModal('myModal${offer}')" class="color${offer}">Click To Schedule</button>`;
         document.querySelector('#scheduleBtn').style = 'transform:scale(.9)'
-    }, radioList.length*300)
-    
-    setTimeout(()=>{
+    }, radioList.length * 300)
+
+    setTimeout(() => {
         document.querySelector('#scheduleBtn').style = 'transform:scale(1)';
-    }, radioList.length*300+250)
+    }, radioList.length * 300 + 250)
 
 };
 
 handleOffer(class65)
 
-{/* <svg class="logo">
-            <polygon fill="teal" points="
-                294.5,155 
-                250,162 
-                290.5,157 
-                311.5,150 
-                322,140 
-                333,125 
-                337,122 
-                345,120 
-                365,119.5 
-                366,110
-                355,110
-                343,110
-                332.6,110.5
-                325,119
-                304,145
-            "></polygon>
-        </svg> */}
+const showSyllabus = () => {
+    if (document.getElementById('syllabus').innerHTML.length) {
+        document.querySelector('#syllabus').style.transform = 'scaleY(0%)'
+        setTimeout(() => {
+            document.getElementById('syllabus').innerHTML = '';
+        }, 1000);
+    } else {
+        document.querySelector('#syllabus').innerHTML = `
+
+    };
+        <div>
+        <h3>Data Analytics:</h3>
+        Intermediate Excel
+        <ul>
+            <li> Pivot Tables</li>
+            <li> VBA Scripting</li>
+        </ul>
+        Fundamental Statistics
+        <ul>
+            <li> Modelling</li>
+            <li> Forecast</li>
+        </ul>
+        Python Programming
+        <ul>
+            <li> Python 3</li>
+            <li> NumPy</li>
+            <li> Pandas</li>
+            <li> Matplotlib</li>
+            <li> API Interactions</li>
+        </ul>
+        Databases
+        <ul>
+            <li> Postgres/pgAdmin</li>
+            <li> MongoDB</li>
+            <li> (ETL) <br> Extract-Transform-Load</li>
+        </ul>
+
+        Front End Web Visualization
+        <ul>
+            <li> HTML</li>
+            <li> CSS</li>
+            <li> Bootstrap</li>
+            <li> Dashboarding</li>
+            <li> JavaScript</li>
+            <li> Geomapping with <br> JavaScript libraries</li>
+        </ul>
+
+        Business Intelligence Software
+        <ul>
+            <li> Tableau</li>
+        </ul>
+
+        Advanced Topics
+        <ul>
+            <li> Big Data Analytics with Hadoop</li>
+            <li> Amazon Web Services</li>
+            <li> Machine Learning</li>
+        </ul>
+
+        </div>
+        <div>
+        <h3>Web Developer:</h3>
+        Computer Science applied to JavaScript
+        <ul>
+            <li> Design Patterns</li>
+            <li> Data Structures</li>
+            <li> Algorithms</li>
+            <li> Big O Notation</li>
+        </ul>
+
+        Browser Based Technologies
+        <ul>
+            <li> HTML</li>
+            <li> CSS</li>
+            <li> JavaScript</li>
+            <li> jQuery</li>
+            <li> Responsive Design</li>
+            <li> Bootstrap</li>
+            <li> Handlebars</li>
+            <li> Local Storage,<br> Session Storage, IndexedDB</li>
+            <li> React.js</li>
+        </ul>
+
+        Deployment
+        <ul>
+            <li> Heroku</li>
+            <li> Git</li>
+        </ul>
+
+        Locally Popular MVC Framework
+        <ul>
+            <li> C#/ASP.NET</li>
+            <li> Python/Django</li>
+            <li> Java/Spring</li>
+            <li> PHP/Laravel</li>
+        </ul>
+
+        Databases
+        <ul>
+            <li> MySQL</li>
+            <li> MongoDB</li>
+        </ul>
+
+        Server-Side Development
+        <ul>
+            <li> Express</li>
+            <li> User Authentication</li>
+            <li> Sequelize</li>
+            <li> MERN Stack <br>(MongoDB, Express.js, <br> React.js, Node.js)</li>
+        </ul>
+
+        Quality and Performance
+        <ul>
+            <li> Unit Testing</li>
+            <li> Google Lighthouse</li>
+            <li> Webpack</li>
+            <li> PWAs (Progressive Web Apps)</li>
+            <li> Lazy Loading</li>
+            <li> Service Workers</li>
+        </ul>
+
+        Accessibility
+        <ul>
+            <li> Semantic HTML</li>
+        </ul>
+        </div>
+    `;
+        document.querySelector('#syllabus').style.transform = 'scaleY(100%)';
+    };
+};
